eb01e603bee6d1a3017ee54a618347595347330c
wisixicidi <wisixicidi@gmail.com>
2023-02-16 13:25:30 +0000

    .

--- back/run.py
+++ back/run.py
@@ -1,26 +1,28 @@
 from settings import HOST,PORT,RELOAD,WORKERS
-from fastapi import FastAPI,Request,BackgroundTasks
+from fastapi import FastAPI,Request
 from source import main_router
 from loguru import logger
-from utils import request_parse,response_parse
+from utils import request_parse,response_parse,make_log
 
 import asyncio
 import uvicorn
+import sys
 
 app = FastAPI()
 app.include_router(main_router)
 
 @app.on_event("startup")
 async def init():
-    logger.add("log/BackendServer_{time:YYYY-MM-DD}.log",format="<green>{time:YYYY-MM-DD HH:mm:ss}</green>\n<blue>{message}</blue>\n")
+    logger.add("log/BackendServer_{time:YYYY-MM-DD}.log",format="<green>{time:YYYY-MM-DD HH:mm:ss}</green>\n<blue>{message}</blue>\n",rotation="1 days",enqueue=True)
     
 
 @app.middleware("http")
 async def watch_log(request: Request, call_next,):
-    start_log = request_parse(request)
+    start_log,start_time = request_parse(request)
     response = await call_next(request)
-    end_log = response_parse(response)
-    logger.info(start_log+"\n"+end_log)
+    end_log,end_time = response_parse(response)
+    log = make_log(start_log,end_log,start_time,end_time)
+    logger.info(log,)
     return response
 
 if __name__=="__main__":
--- back/utils.py
+++ back/utils.py
@@ -1,7 +1,10 @@
-
+import time
 
 def request_parse(request):
-    return f"\tMethod : {request.method}\n\t여기에서 : {request.client.host}:{request.client.port}\n\t여기로 : {request.url}"
+    return f"\tMethod : {request.method}\n\t여기에서 : {request.client.host}:{request.client.port}\n\t여기로 : {request.url}",time.time()
 
 def response_parse(response):
-    return f"\t응답코드 : {response.status_code}"
+    return f"\t응답코드 : {response.status_code}\n\t결과타입 : {response.headers['content-type']}",time.time()
+
+def make_log(start_log,end_log,start_time,end_time):
+    return start_log+"\n"+end_log+f"\n\t처리시간 : {str(end_time-start_time)[:7]}"
