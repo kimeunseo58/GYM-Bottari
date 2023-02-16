8922dfefe712c1ba3cc6adb3773c3bdedc4a77e5
wisixicidi <wisixicidi@gmail.com>
2023-02-16 12:26:54 +0000

    loguru 추가

--- back/run.py
+++ back/run.py
@@ -1,8 +1,8 @@
-from settings import HOST,PORT
-from fastapi import FastAPI,Request
+from settings import HOST,PORT,RELOAD,WORKERS
+from fastapi import FastAPI,Request,BackgroundTasks
 from source import main_router
 from loguru import logger
-from utils import request_parse
+from utils import request_parse,response_parse
 
 import asyncio
 import uvicorn
@@ -11,18 +11,23 @@ app = FastAPI()
 app.include_router(main_router)
 
 @app.on_event("startup")
-async def rm_all_dockers():
+async def init():
     logger.add("log/BackendServer_{time:YYYY-MM-DD}.log",format="<green>{time:YYYY-MM-DD HH:mm:ss}</green>\n<blue>{message}</blue>\n")
-    logger.info(f"Server Start\n")
     
 
 @app.middleware("http")
-async def watch_log(request: Request, call_next):
-    logger.info(request_parse(request))
+async def watch_log(request: Request, call_next,):
+    start_log = request_parse(request)
     response = await call_next(request)
+    end_log = response_parse(response)
+    logger.info(start_log+"\n"+end_log)
     return response
 
-
-
 if __name__=="__main__":
-    asyncio.run(uvicorn.run(app=app,host=HOST,port=PORT))
+    asyncio.run(uvicorn.run(
+        "run:app",
+        host=HOST,
+        port=PORT,
+        workers=WORKERS,
+        reload=RELOAD,
+        ))
--- back/utils.py
+++ back/utils.py
@@ -1,5 +1,7 @@
 
 
-
 def request_parse(request):
-    return f"{request.method} To {request.url} From {request.client.host}:{request.client.port}"
+    return f"\tMethod : {request.method}\n\t여기에서 : {request.client.host}:{request.client.port}\n\t여기로 : {request.url}"
+
+def response_parse(response):
+    return f"\t응답코드 : {response.status_code}"
